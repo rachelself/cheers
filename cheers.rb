d51171f0c96dc3c62fbcf884bfff531627cde134
Rachel Self <rachel.m.self@gmail.com>
2014-07-07 18:54:25 -0500

    finishing up bday calculations

--- cheers.rb
+++ cheers.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 puts "What is your name?"
 name = gets.chomp
 if name.empty?
@@ -15,12 +17,80 @@ else
   end
   puts "#{name}\'s just GRAND"
 
-  puts "Hey #{name}, when is your birthday?"
+  puts "Hey #{name}, when is your birthday? ...(yyyy-mm-dd)"
   birthday = gets.chomp
   if birthday.empty?
     puts "You must give me your bday"
   else
-    puts "Your bday is... #{birthday}"
+    puts "Your bday is #{birthday}"
+    # bday = Date.parse("#{birthday}")
+    # puts "bday: #{bday}"
+    # now = Date.today
+    # puts "today: #{now}"
+
+    today = Date.today.yday
+    puts "today's yday: #{today}"
+
+    bday = Date.parse("#{birthday}").yday
+    puts "bday's yday: #{bday}"
+
+    if today == bday
+      puts "Happy Birthday!"
+    elsif today > bday
+      # diff = 365 - 
+      # puts "Your birthday is in "
+
+
+    # n = now.year
+    # c = Date.new(n, bday.month, bday.day)
+    # puts "compare date: #{c}"
+    # # t = Date.new()
+    # # puts "test date: #{t}"
+    #
+    # if c == bday
+    #   puts "Happy Birthday!"
+    # else
+    #   diff = now - c
+    #   puts "diff: #{diff}"
+    # end
+
+
+
+
+    # ydiff = now.year - bday.year
+    # mdiff = now.month - bday.month
+    # ddiff = now.day - bday.day
+    #
+    #
+    # if ydiff > 0
+    #   puts "diff: #{ydiff}"
+    #   if mdiff == 0
+    #     puts "mdiff: #{mdiff}"
+    #     puts "It is your bday month!"
+    #     if ddiff == 0
+    #       puts "Happy Birthday!"
+    #     else
+    #       if ddiff == 1
+    #         puts "Your birthday is tomorrow!"
+    #       else
+    #         puts "Your birthday is in #{ddiff} days"
+    #   else
+    #     puts "It is not yet your bday month"
+    #     puts "mdiff: #{mdiff}"
+    #     # mdays = mdiff * 3
+    #
+    # else
+    #   puts "You are not old enough to have a birthday yet"
+
+
+
+    # c = Date.new("#{bday}.month")
+
+
+    # d = Date._strptime("#{birthday}", "%Y-%-m-%-d")
+    # puts "bday: #{d}"
+    # puts "date: #{bday}"
+
   end
 
 end
